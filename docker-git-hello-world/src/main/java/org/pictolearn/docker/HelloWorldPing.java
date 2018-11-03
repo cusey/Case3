0ca7ac798c10ccef7e7e791e73216d2441bc05b2
John Cusey <jjcusey@gmail.com>
2018-11-03 16:23:09 -0400

    Fixed Image

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -1,16 +1,11 @@
-package org.cusey.john;
+package org.pictolearn.docker;
 
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		
-		for(int i = 0 ; i < 100000; i++){
-			
+		for(int i = 0 ; i < 100; i++){
 			System.out.println("Hello World Ping " + i );
-			
 			Thread.sleep(1000);
-			
 		}
 	}
-
 }
