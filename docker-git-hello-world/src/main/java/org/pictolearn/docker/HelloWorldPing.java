d28c2b35ea7ee1ac9e377b66bdb5093f46ccaf9a
John Cusey <jjcusey@gmail.com>
2018-11-03 16:43:38 -0400

    Update docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+		for(int i = 0 ; i < 10000; i++){
+			System.out.println("Hi Ping " + i );
 			Thread.sleep(1000);
 		}
 	}
